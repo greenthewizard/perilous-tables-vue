0dff02529e7a7e3a80e106ff8c5c458d7079ee75
buddytherat <buddytherat@gmail.com>
2018-06-20 21:22:21 -0500

    Added basic box component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <h1>Perilous Tables</h1>
-    
+    <div class="grid-container">
+      <pt-box></pt-box>
+    </div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import PtBox from './components/PtBox.vue'
 
 export default {
   name: 'app',
   components: {
-    // HelloWorld
+    PtBox
   }
 }
 </script>
@@ -19,4 +21,40 @@ export default {
 <style lang="sass">
 @import './assets/styles/normalize'
 @import './assets/styles/base'
+@import url('https://fonts.googleapis.com/css?family=Roboto')
+@import './assets/styles/colors'
+
+body
+    font-family: 'Roboto', sans-serif
+    color: $grey
+
+h1
+    margin: .5rem
+    color: $lightgrey
+
+.grid-container 
+  width: 80vw
+  max-width: 90rem
+  min-width: 30rem
+  margin: 3rem auto
+  display: grid
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr))
+  grid-gap: .7rem
+
+.l-flexbox
+  display: flex
+.l-flexitem--end-cap
+  margin-left: auto
+.l-flexitem--padded
+  padding: 0 1.2rem 0 0
+
+.grid-section-divider
+  grid-column: 1 / -1
+  border-bottom: 1px solid $lightgrey
+.grid-section-divider__header
+  font-size: 2.3rem
+  color: $lightgrey
+  padding-bottom: 0
+.grid-section-divider--collapsed
+  color: #bad455
 </style>
---
+++ src/components/PtBox.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="pt-box">
+        <header class="pt-box__header">
+            <div class="pt-box__title-card">
+                <div class="l-flexbox">
+                    <div class="l-flexitem">
+                        <h2 class="pt-box__title">{{ title }}</h2>
+                        <h3 class="pt-box__sub-title">{{ subTitle }}</h3>
+                    </div>
+                    <div class="l-flexitem l-flexitem--end-cap">
+                        <div class="pt-box__title-btn">
+                            <input 
+                                class="icon"
+                                type="image"
+                                alt="dice"
+                                :src="require('../assets/svg/dice.svg')">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div class="pt-box__content">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            title: 'Generate',
+            subTitle: 'Draw maps, leave blanks...'
+        }
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '../assets/styles/colors'
+
+.pt-box 
+    position: relative
+    border: 1px solid black
+    border-radius: 8px
+    padding: 1rem
+
+    h2
+        font-size: 2.2rem
+
+    &:hover
+        .pt-box__content
+            p
+                color: $lightblue
+    
+.pt-box__header
+    padding: 0 0 .5rem 0
+.pt-box__title
+    margin: 0
+.pt-box__title--highlight
+    color: $lightblue
+.pt-box__sub-title
+    font-weight: normal
+    font-size: 1.3rem
+    color: $lightgrey
+    margin: 0
+.icon
+    width: 4rem
+    height: 4rem
+    outline: none
+</style>
