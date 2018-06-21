0515e7ed6b5b5af8977ca3638d7621448f72cafd
buddytherat <buddytherat@gmail.com>
2018-06-20 20:41:04 -0500

    Added base sass/scss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>Perilous Tables</h1>
-
+    
   </div>
 </template>
 
@@ -16,13 +16,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import './assets/styles/normalize'
+@import './assets/styles/base'
 </style>
