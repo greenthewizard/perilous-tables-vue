1526fd8e9898b59576ca13c3e8ead790c708a95a
buddytherat <buddytherat@gmail.com>
2018-07-23 22:09:48 -0500

    Added empty example file for box components

---
+++ src/components/BlankBox.vue
@@ -0,0 +1,29 @@
+<template>
+    <pt-box
+        title="Title"
+        subTitle="Subtitle">
+        <template slot="titleButtons">
+        </template>
+        <template slot="content">
+        </template>
+    </pt-box>
+</template>
+
+<script>
+//Mixin
+import boxMixin from '../mixins/boxMixin';
+
+//Components
+// import YourComponent from '../YourComponent.vue;
+
+export default {
+    mixins: [boxMixin],
+    components: {
+        // YourComponent
+    }
+}
+</script>
+
+<style language="sass" scoped>
+
+</style>
