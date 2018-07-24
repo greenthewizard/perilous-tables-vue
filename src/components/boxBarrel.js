a7b31806b07feee6ae50f333f99c3ef8a0f3930d
buddytherat <buddytherat@gmail.com>
2018-07-23 22:12:40 -0500

    Added GenDungeon component to App.vue imports

--- src/components/boxBarrel.js
+++ src/components/boxBarrel.js
@@ -1,7 +1,9 @@
 import NavMain from './nav/NavMain.vue';
 import NavGenerate from './nav/NavGenerate.vue';
+import GenDungeon from './nav/GenDungeon.vue';
 
 export default {
     NavMain,
-    NavGenerate
+    NavGenerate,
+    GenDungeon
 }
